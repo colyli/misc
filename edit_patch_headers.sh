8bc40ae6d88c53088ba045a4fb10ea19214f884a
Coly Li <colyli@suse.de>
2016-04-25 11:18:23 +0800

    A script to modify kenrel patch headers with other scripts in suse kernel tree.

---
+++ edit_patch_headers.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -x
+
+linux_path=~/source/linux/linux
+reference="bsc#953048"
+
+for p in `cat patchlist`;do
+	./scripts/patch-tags-from-git $p $linux_path
+	./scripts/patch-tag --Add Signed-off-by="Coly Li <colyli@suse.de>" $p 
+	./scripts/patch-tag --add Reference=$reference $p
+	./scripts/patch-tag --delete Mime-version --delete Content-type --delete Content-transfer-encoding $p
+done
