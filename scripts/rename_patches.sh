63aa1dd58e04e7154e4d1024c3cefe878ad4f296
Coly Li <colyli@suse.de>
2017-03-26 14:42:43 +0800

    remove patch seq ID if there is

--- scripts/rename_patches.sh
+++ scripts/rename_patches.sh
@@ -10,12 +10,14 @@ list=$1
 
 nr=1
 for p in `cat $list`;do
+	p=`echo $p|sed "s/^[0-9]\{4\}-//"`
 	f=`ls *"$p"*`
 	if [ $? != 0 ];then
 		echo "$p does not exist"
 		continue
 	fi
 
+	# in case $f has seq ID
 	id=`printf %.4d $nr`
 	nr=`expr $nr + 1`
 	name=$id"-"$p
